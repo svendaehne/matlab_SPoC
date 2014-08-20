24e0f1fff284cdde0a6a707838a3857390e8cce6
SvenDaehne <SvenDaehne@1449bcba-80d8-4576-a99e-0f548206d62b>
2014-08-20 13:10:00 +0000

    Change in implementation to speed up create_Cxxz.m
    git-svn-id: https://repo.ml.tu-berlin.de/svn/ida/public/bbci/investigation/methods/SSCA/public@37710 1449bcba-80d8-4576-a99e-0f548206d62b

--- utils/create_Cxxz.m
+++ utils/create_Cxxz.m
@@ -2,9 +2,9 @@ function Cxxz = create_Cxxz(Cxxe, z)
 % Creates the z-weighted covariance matrix from a time series of covariance
 % matrices given in Cxxe 
 
-N_e = size(Cxxe,3);
-Z = zeros(size(Cxxe));
-for e=1:N_e
-    Z(:,:,e) = z(e);
-end
-Cxxz = sum(Cxxe.*Z , 3) ./ N_e; 
+[Nx, ~, Ne] = size(Cxxe);
+% vectorize the trialwise covariance time series
+Cxxe_vec = reshape(Cxxe, [Nx*Nx, Ne]);
+% compute the z-weighted average covariance matrix via matrix-vector multiplication
+Cxxz = reshape(Cxxe_vec * z', [Nx, Nx]) / Ne;
+ 
