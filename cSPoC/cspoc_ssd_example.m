e3991d175f16b1ec39cf18b381644bb59d5a4502
SvenDaehne <SvenDaehne@1449bcba-80d8-4576-a99e-0f548206d62b>
2015-03-03 17:29:55 +0000

    modified cSPoC example scripts
    git-svn-id: https://repo.ml.tu-berlin.de/svn/ida/public/bbci/investigation/methods/SSCA/public@39573 1449bcba-80d8-4576-a99e-0f548206d62b

--- cSPoC/cspoc_ssd_example.m
+++ cSPoC/cspoc_ssd_example.m
@@ -1,31 +1,37 @@
-% A small example that illustrates how cSPoC works and how it can be
-% applied.
 %
-% In this example we assume that we have some datasets X{n} for n in {1,..,N}, 
-% each with a certain number M{n} of channels that measure a mixture of source 
-% activity. The envelope of K sources in the datasets are coupled and 
+% A small example that illustrates how cSPoC works and how it can be
+% applied in conjunction with SSD (or any other linear method)
+% preprocessing.
+%
+% In this example we assume that we have N datasets, each with
+% a certain number of channels that measure a mixture of source 
+% activity. The envelope of K sources in each dataset is coupled to a 
+% corresponding sources in the other datasets,
 % these are the "target sources" that we seek to extract with cSPoC.
 %
 % Note that there is training and test data! 
 %
 % Run this script a few times to get a feeling for the influence of the
-% paramters (SNR, Ns, Ne_tr, etc).
+% paramters (SNR, N, Ne_tr, etc).
 
 
 %% parameters
 
-N = 3; % number of datasets
+N = 3; % number of datasets, e.g. subjects
 K = 2; % number of "target source pairs", i.e. sources with coupled envelopes
-n_sources = K + (1:N); % number of total simulated sources per dataset
-n_channels = max(15,K) + (1:N); % number of channels per dataset
+n_sources = K + 10 + (1:N); % number of total simulated sources per dataset
 
-band = [8,12]; % frequency band of interest here
+n_channels = 100 + (1:N); % number of channels per dataset
+
+n_ssd_components = 20; % number of SSD components used for dim-reduction
+
+band = [8,12]; % frequency band of simulated data
+
 SNR = 0.5; % signal-to-noise ratio (between 0 and 1) in terms of variance explained by the target source
-
-Ne_tr = 50; % number of training epochs/trials
-Ne_te = 50; % number of test epochs/trials
+Ne_tr = 250; % number of training epochs/trials
+Ne_te = 250; % number of test epochs/trials
 Ne = Ne_tr + Ne_te;
-Te = 100; % number of samples per epoch
+Te = 50; % number of samples per epoch
 samples_per_second = 100;
 
 tr_idx = 1:Ne_tr;
@@ -34,11 +40,6 @@ te_idx = (1:Ne_te) + Ne_tr;
 % make sure the SNR is between 0 and 1
 SNR = max(0,SNR);
 SNR = min(1,SNR);
-
-% S = cell(1,N); % source signals (time courses)
-% S_env = cell(1,N); % envelopes of source signals
-% X = cell(1,N); % sensor signals
-
 
 fprintf('\n')
 fprintf('--- cSPoC example ---\n')
@@ -58,36 +59,33 @@ fprintf(' %d\n', n_channels(N));
 
 fprintf('\n')
 
-
 %% create example data
 
 fprintf('creating data \n')
 [X, S, S_env, A_true] = create_cspoc_example_data(N, K, n_sources, n_channels,... 
                         SNR, samples_per_second, Ne, Te, band);
 
-% plot source signals
-figure
-for n=1:N
-    subplot(N,1,n)
-    plot_time_courses(zscore(S{n}), 1,'s',1:K);
-    title(sprintf('dataset #%d in source space',n))
-    xlabel('time')
-    set(gca, 'xtickLabel',[])
-    ylabel('sources')
-end
-pause(0.1)
+%% apply SSD for dimensionality reduction
 
-% plot sensor signals
-figure
+fprintf('applying SSD for dimensionality reduction\n')
+freq = [band; band+[-2,2]; band+[-1,1]];
 for n=1:N
-    subplot(N,1,n)
-    plot_time_courses(zscore(X{n}), 1,'x');
-    title(sprintf('dataset #%d in sensor space', n))
-    xlabel('time')
-    set(gca, 'xtickLabel',[])
-    ylabel('sensors')
+
+    % compute filters and patterns from SSD
+    [W_ssd, A_ssd] = ssd(X{n}, freq, samples_per_second,[],[]);
+
+    % denoise by projecting onto a subset of components
+    X{n} = denoising_by_low_rank_factorization(...
+            W_ssd, A_ssd, X{n}, n_ssd_components);
+    
+    % IMPORTANT!!!
+    % If X is broadband, don't forget to band-pass filter it here! 
+    %  X{n} = bandpass_filter(...);
+    % In this example X is already band-limited because of the way it was
+    % generated, so this step is not necessary.
+    
 end
-pause(0.5)
+
 
 %% reshape sensor signals into epochs
 
@@ -96,8 +94,9 @@ X_epo_tr = cell(1,N);
 X_epo_te = cell(1,N);
 
 for n=1:N
+
     % create epoched data structure
-    X_epo{n} = permute(reshape(X{n}, [Te, Ne, n_channels(n)]), [1,3,2]);
+    X_epo{n} = permute(reshape(X{n}, [Te, Ne, size(X{n},2)]), [1,3,2]);
     
     % split training and test data
     X_epo_tr{n} = X_epo{n}(:,:,tr_idx);
@@ -105,25 +104,20 @@ for n=1:N
 end
 
 %% unmix with cSPoC, only using training data
+
 fprintf('\n\nbegin extracting envelope-correlated sources with cSPoC ... \n')
 
 [W, A] = cspoc(X_epo_tr,1, ...
     'n_component_sets', K+1, ... % number of component pairs to be extracted
-    'average_over_epochs', 0, ...
+    'average_over_epochs', 1, ...
     'n_repeats', 5, ... % number of re-starts per component pair
     'verbose', 2 ...
     );
 
 fprintf('\ndone!\n')
 
-%% project data to cSPoC components and compute envelopes 
 
-R = zscore(A{1}(:,1:K))'* zscore(A_true{1}(:,1:K));
-[~,idx] = max(abs(R));
-for n=1:N
-    W{n}(:,1:length(idx)) = W{n}(:,idx);
-    A{n}(:,1:length(idx)) = A{n}(:,idx);
-end
+%% project data to cSPoC components and compute envelopes 
 
 S_est = cell(1,N);
 phi = cell(1,N);
@@ -150,7 +144,7 @@ for k=1:rows
         plot(phi{n}(:,k), 'color', colors(n,:), 'linewidth',2)
         legend_string{n} = sprintf('source extracted from dataset #%d',n);
     end
-    ylim([-4,5])
+    ylim([-5,6])
     box on
     legend(lh,legend_string)
     title(sprintf('time-course of extracted source pair #%d', k))
@@ -160,17 +154,15 @@ for k=1:rows
     text(Ne_tr*Te*1.01, 4, '--> test data')
     h=text(Ne_tr*Te*0.99, 4, 'training data <--');
     set(h, 'HorizontalAlignment', 'right');
-end
+end    
 
 %% plot spatial patterns
 figure
-rows = K; 
-% rows = size(W{1},2);
+rows = size(W{1},2);
 cols = N;
-for k=1:rows % components
-    for n=1:cols % datasets
-        plot_idx = n + (k-1)*cols;
-        subplot(rows,cols, plot_idx)
+for k=1:rows
+    for n=1:N
+        subplot(rows,cols, sub2ind([rows,cols],n,k))
         a_true = A_true{n}(:,k) / norm(A_true{n}(:,k));
         a = A{n}(:,k) / norm(A{n}(:,k));
         a = a * sign(corr(a, a_true));
